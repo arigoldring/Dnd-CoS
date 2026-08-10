1c7134a7f2548d8104264599ca61ed2cf04aea45
Dlizard72 <arigoldring77@gmail.com>
2026-08-10 13:09:06 -0400

    Updates the error message the user receives when items fail to load

--- strahd-hub/src/hooks/useItems.ts
+++ strahd-hub/src/hooks/useItems.ts
@@ -1,6 +1,13 @@
+import { errorMessage } from "../lib/errors";
 import { getItems } from "../services/items";
 import { useQuery } from "@tanstack/react-query";
 
 export function useItems() {
-  return useQuery({ queryKey: ["items"], queryFn: getItems });
+  return useQuery({
+    queryKey: ["items"],
+    queryFn: () =>
+      getItems().catch((err) => {
+        throw new Error(errorMessage(err, "Failed to load items"));
+      }),
+  });
 }
